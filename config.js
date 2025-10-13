a8ce3a7517b0d131b5ede432f144acb3e832657b
Arman Marabyan <armangolden75@gmail.com>
2025-10-13 23:27:28 +0900

    Use Supabase Transaction pooler connection

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ module.exports = {
   TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN || '8484396546:AAGu4qusRZnvKPoF8leX5C8trbWh_xeELME',
   TELEGRAM_WEBAPP_URL: process.env.TELEGRAM_WEBAPP_URL || 'https://your-domain.com',
   // Supabase PostgreSQL connection
-  POSTGRES_URL: process.env.POSTGRES_URL || 'postgresql://postgres:Hayastandjan89@db.vvuodxabzeudqskteiiz.supabase.co:5432/postgres'
+  POSTGRES_URL: process.env.POSTGRES_URL || 'postgresql://postgres.vvuodxabzeudqskteiiz:Hayastandjan89@aws-1-us-east-2.pooler.supabase.com:6543/postgres'
 };
